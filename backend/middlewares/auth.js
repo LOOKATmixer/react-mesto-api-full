6aabf3f099dd22056da6fcfa019ab28896f4ef81
LOOKATmixer <65297330+LOOKATmixer@users.noreply.github.com>
2021-02-24 14:13:25 +0500

    p15.i1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,11 +10,7 @@ const extractBearerToken = (header) => header.replace('Bearer ', '');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-<<<<<<< HEAD
-    throw new UnauthError('Необходимо авторизироваться1');
-=======
     throw new UnauthError('Необходимо авторизироваться');
->>>>>>> 40fe695abbf0ea60b1d869f3012387fffe103a27
   }
 
   const token = extractBearerToken(authorization);
@@ -26,11 +22,7 @@ module.exports = (req, res, next) => {
       `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`
     );
   } catch (err) {
-<<<<<<< HEAD
-    throw new UnauthError('Необходимо авторизироваться2');
-=======
     throw new UnauthError('Необходимо авторизироваться');
->>>>>>> 40fe695abbf0ea60b1d869f3012387fffe103a27
   }
 
   req.user = payload;
