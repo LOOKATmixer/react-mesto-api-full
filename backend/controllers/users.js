112f091c8bba07a50d11ab1a45a50e8669cb8531
LOOKATmixer <65297330+LOOKATmixer@users.noreply.github.com>
2021-02-26 11:04:25 +0500

    p15.i2

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -85,7 +85,7 @@ const createUser = (req, res, next) => {
 
 const updateProfile = (req, res, next) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true }, { runValidators: true })
     .then((user) => {
       res.send(user);
     })
@@ -94,7 +94,7 @@ const updateProfile = (req, res, next) => {
 
 const updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true }, { runValidators: true })
     .then((user) => {
       if (!user) {
         throw new NotFoundError('Нет пользователя с таким id');
