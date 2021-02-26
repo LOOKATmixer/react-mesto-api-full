a46f34c6890ca337c929de780ef686aa51d3cc01
LOOKATmixer <65297330+LOOKATmixer@users.noreply.github.com>
2021-02-26 10:41:47 +0500

    p15.i2

--- backend/app.js
+++ backend/app.js
@@ -9,15 +9,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-const rateLimit = require('express-rate-limit');
-
-const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
-});
-
-const helmet = require('helmet');
-
 const { errors, celebrate, Joi, CelebrateError } = require('celebrate');
 const validator = require('validator');
 
@@ -29,6 +20,7 @@ const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
+const app = express();
 
 const validateUserLogin = celebrate({
   body: Joi.object().keys({
@@ -52,17 +44,10 @@ const validateUserSignup = celebrate({
   }),
 });
 
-const app = express();
-
 app.use(cors());
-app.options('*', cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/api/', apiLimiter);
-
-app.use(helmet());
 
 app.use(requestLogger);
 
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -74,6 +74,8 @@ const createUser = (req, res, next) => {
       })
       .catch((err) => {
         if (err.name === 'ValidationError') {
+          next(new BadRequestError('Введены некорректные данные'));
+        } else if (err.code === 11000) {
           next(new ConflictError('Данный email уже зарегистрирован'));
         }
         next(err);
@@ -83,7 +85,7 @@ const createUser = (req, res, next) => {
 
 const updateProfile = (req, res, next) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => {
       res.send(user);
     })
@@ -92,7 +94,7 @@ const updateProfile = (req, res, next) => {
 
 const updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((user) => {
       if (!user) {
         throw new NotFoundError('Нет пользователя с таким id');
@@ -117,7 +119,7 @@ const login = (req, res, next) => {
       );
       res.send({ token });
     })
-    .catch(next);
+    .catch(() => next(new BadRequestError('Введены неверное имя или пароль')));
 };
 
 module.exports = {
